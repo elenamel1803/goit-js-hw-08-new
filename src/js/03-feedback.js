e096379a45e1fd4b2b6223d31bd20e14cd9593e5
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2023-11-07 22:23:55 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  formEl: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('.feedback-form input'),
+  messageTextarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.formEl.addEventListener('input', throttle(onFormInput, 500));
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+addForm();
+
+function onFormInput() {
+  const currentData = {
+    email: refs.emailInput.value,
+    message: refs.messageTextarea.value,
+  };
+  console.log(currentData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function addForm() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+  refs.emailInput.value = savedData.email || '';
+  refs.messageTextarea.value = savedData.message || '';
+}
