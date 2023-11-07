459ea90b364798fa0d6b79dee4737260c4aa646d
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2023-11-07 23:14:16 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>`
+  )
+  .join('');
+listEl.insertAdjacentHTML('beforeend', markupGallery);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(lightbox);
