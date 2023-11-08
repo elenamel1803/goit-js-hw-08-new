3031c01a91fbbc6d5b782e435505a2e261538a63
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2023-11-08 02:02:08 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const vimeoPlayer = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+vimeoPlayer.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+if (savedTime) {
+  vimeoPlayer.setCurrentTime(savedTime);
+}
